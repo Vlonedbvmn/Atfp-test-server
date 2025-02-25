b4c6a5d74477029e21730561a6b78a9f1d7eec4b
Vlonedbvmn <121385436+Vlonedbvmn@users.noreply.github.com>
2025-02-25 23:52:38 +0200

    Add files via upload

---
+++ ATFP.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import os
+
+
+
+st.set_page_config(
+    page_title="ATFP",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+
+if all(key not in st.session_state.keys() for key in ('model', 'num_features', 'score')):
+    st.session_state['num_features'] = []
+    st.session_state['score'] = []
+    st.session_state.clicked = False
+    st.session_state.clicked2 = False
+    st.session_state.df = None
+    st.session_state.date = ""
+    st.session_state.target = ""
+
+
+def display_df():
+    df = pd.DataFrame({"Model": st.session_state['model'],
+                       "Number of features": st.session_state['num_features'],
+                       "F1-Score": st.session_state['score']})
+
+    sorted_df = df.sort_values(by=['F1-Score'], ascending=False).reset_index(drop=True)
+
+    st.write(sorted_df)
+
+
+if __name__ == "__main__":
+    st.title("ATFP - AI Timeseries Forecasting Platform")
+
+    st.subheader(
+        "Вітаю, це сторінка проєкту, який є програмним застосунком для дослідників у сфері прогнозування часових рядів використовуючи методи машинного навчання. Для початку роботи натисніть розділ Дані, щоб програма отримала дані з якими ви хочете працювати та отримувати прогнози.")
+    video_f = open("instruction_2.mp4", "rb")
+    video_bytes = video_f.read()
+    st.subheader(" ")
+    st.divider()
+    st.subheader("Відео інструкція користування застосунком")
+    st.video(video_bytes)
---
+++ start.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import pandas as pd
+import os
+
+os.environ['NIXTLA_ID_AS_COL'] = '1'
+
+if 'role' not in st.session_state:
+    st.session_state.role = None
+# Directory of the pages
+# st.logo("atfp-logo.png")
+st.set_page_config(
+    # page_title="ATFP",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+
+
+st.logo("atfp-logo-rgb.png")
+
+st.html("""
+  <style>
+    [alt=Logo] {
+      height: 3rem;
+    }
+  </style>
+        """)
+
+p1 = st.Page(
+    "pa/ATFP.py",
+    title="Головна",
+)
+p2 = st.Page(
+    "pa/Дані.py",
+    title="Дані",
+)
+p3 = st.Page(
+    "pa/Налаштування моделі.py",
+    title="Налаштування моделі",
+)
+
+p4 = st.Page(
+    "pa/Прогноз.py",
+    title="Прогноз",
+)
+p5 = st.Page(
+    "pa/Тест на аномалії.py",
+    title="Тест на аномалії",
+)
+p6 = st.Page(
+    "pa/ШІ помічник.py",
+    title="ШІ помічник",
+)
+p7 = st.Page(
+    "pa/Порівняти.py",
+    title="Порівняти",
+)
+
+# st.title("ATFP - AI Timeseries Forecasting Platform")
+#
+# st.subheader(
+#     "Вітаю, це сторінка проєкту, який є програмним застосунком для дослідників у сфері прогнозування часових рядів використовуючи методи машинного навчання. Для початку роботи натисніть розділ Дані, щоб програма отримала дані з якими ви хочете працювати та отримувати прогнози.")
+# video_f = open("instruction_2.mp4", "rb")
+# video_bytes = video_f.read()
+# st.subheader(" ")
+# st.divider()
+# st.subheader("Відео інструкція користування застосунком")
+# st.video(video_bytes)
+
+
+
+# if st.session_state.role == "Аматор або професіонал":
+pg = st.navigation({"":[p1, p2], "Для фахівців:":[p3, p4, p7, p5], "Для всіх:":[p6]})
+pg.run()
+
+# if st.session_state.role == "Новачок":
+#     pg = st.navigation([p1, p2, p6])
+#     pg.run()
+
+# if st.session_state.role is None:
+    # st.title("Перед користуванням цим застосунком, оберіть хто Ви у сфері прогнозування часових рядів")
+    #
+    # role = st.selectbox("Роль:", ["Аматор або професіонал", "Новачок"])
+    # if st.button("Підтвердити"):
+    #     st.session_state.role = role
+    #     st.rerun()
+
+
+
