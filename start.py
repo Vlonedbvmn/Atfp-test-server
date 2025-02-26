977dde440711c781506a064d43960fd85fea6c11
Vlonedbvmn <121385436+Vlonedbvmn@users.noreply.github.com>
2025-02-26 14:06:27 +0200

    Update start.py

--- start.py
+++ start.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import os
 
+if lang not in st.session_state:
+    st.session_state.lang = "ukr"
+
+
 os.environ['NIXTLA_ID_AS_COL'] = '1'
 
 if 'role' not in st.session_state:
@@ -25,35 +29,6 @@ st.html("""
   </style>
         """)
 
-p1 = st.Page(
-    "pa/ATFP.py",
-    title="Головна",
-)
-p2 = st.Page(
-    "pa/Дані.py",
-    title="Дані",
-)
-p3 = st.Page(
-    "pa/Налаштування моделі.py",
-    title="Налаштування моделі",
-)
-
-p4 = st.Page(
-    "pa/Прогноз.py",
-    title="Прогноз",
-)
-p5 = st.Page(
-    "pa/Тест на аномалії.py",
-    title="Тест на аномалії",
-)
-p6 = st.Page(
-    "pa/ШІ помічник.py",
-    title="ШІ помічник",
-)
-p7 = st.Page(
-    "pa/Порівняти.py",
-    title="Порівняти",
-)
 
 # st.title("ATFP - AI Timeseries Forecasting Platform")
 #
@@ -69,8 +44,87 @@ p7 = st.Page(
 
 
 # if st.session_state.role == "Аматор або професіонал":
-pg = st.navigation({"":[p1, p2], "Для фахівців:":[p3, p4, p7, p5], "Для всіх:":[p6]})
-pg.run()
+if st.session_state.lang == "ukr":
+    selected_language = st.sidebar.selectbox("Оберіть мову:", ["Українська", "English"])
+    if selected_language = "Українська":
+        st.session_state.lang = "ukr"
+    else:
+        st.session_state.lang = "eng"
+else:
+    selected_language = st.sidebar.selectbox("Choose language:", ["Українська", "English"])
+    if selected_language = "Українська":
+        st.session_state.lang = "ukr"
+    else:
+        st.session_state.lang = "eng"
+    
+
+# Load the appropriate translation (assuming your locale files are in the 'locales' folder)
+if st.session_state.lang == "ukr":
+    p1 = st.Page(
+    "pa/ATFP.py",
+    title="Головна",
+    )
+    p2 = st.Page(
+    "pa/Дані.py",
+    title="Дані",
+    )
+    p3 = st.Page(
+    "pa/Налаштування моделі.py",
+    title="Налаштування моделі",
+    )
+
+    p4 = st.Page(
+    "pa/Прогноз.py",
+    title="Прогноз",
+    )
+    p5 = st.Page(
+    "pa/Тест на аномалії.py",
+    title="Тест на аномалії",
+    )
+    p6 = st.Page(
+    "pa/ШІ помічник.py",
+    title="ШІ помічник",
+    )
+    p7 = st.Page(
+    "pa/Порівняти.py",
+    title="Порівняти",
+    )
+
+    pg = st.navigation({"":[p1, p2], "Для фахівців:":[p3, p4, p7, p5], "Для всіх:":[p6]})
+    pg.run()
+else:
+    p1 = st.Page(
+    "pa/ATFP.py",
+    title="Home",
+    )
+    p2 = st.Page(
+    "pa/Дані.py",
+    title="Data",
+    )
+    p3 = st.Page(
+    "pa/Налаштування моделі.py",
+    title="Model",
+    )
+
+    p4 = st.Page(
+    "pa/Прогноз.py",
+    title="Forecast",
+    )
+    p5 = st.Page(
+    "pa/Тест на аномалії.py",
+    title="Anoamly analisis",
+    )
+    p6 = st.Page(
+    "pa/ШІ помічник.py",
+    title="AI assistant",
+    )
+    p7 = st.Page(
+    "pa/Порівняти.py",
+    title="Compare",
+    )
+
+    pg = st.navigation({"":[p1, p2], "Для фахівців:":[p3, p4, p7, p5], "Для всіх:":[p6]})
+    pg.run()
 
 # if st.session_state.role == "Новачок":
 #     pg = st.navigation([p1, p2, p6])
